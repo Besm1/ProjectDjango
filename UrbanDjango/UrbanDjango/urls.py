dd321605b2477e4b30259659fcdd4f6dd00c9700
besm1 <besn1@yandex.ru>
2025-01-10 21:19:36 +0300

    task3 - Базовые HTML тэги в шаблонах

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from task2.views import func_template, ClassTemplate
-
+from task3.views import AutoShop, Service, SpareParts
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', func_template),
-    path('class/', ClassTemplate.as_view())
+    path('admin/', admin.site.urls)
+    , path('', func_template)
+    , path('class/', ClassTemplate.as_view())
+    , path('autoshop/', AutoShop.as_view())
+    , path('autoshop/service/', Service.as_view())
+    , path('autoshop/parts/', SpareParts.as_view())
 ]
